9ed6cfd179269900c0dcf8d6bfe3d31286bde30e
Viktor.Gladkich <viktorgladkich7@gmail.com>
2026-08-08 16:46:57 +0200

    remove GSAP patching scripts and cleanup lenis type definitions

--- patch_gsap2.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = 'src/components/sections/HeroKnockout.tsx';
-let code = fs.readFileSync(file, 'utf8');
-
-// Comment out useGSAP block
-code = code.replace(/useGSAP\(\s*\(\)\s*=>\s*\{[\s\S]*?\}\s*,\s*\{\s*scope:\s*root,\s*dependencies:\s*\[fontSize\]\s*\}\s*\);/g, '/* useGSAP commented out for debugging */');
-
-fs.writeFileSync(file, code);
--- src/types/lenis.d.ts
+++ src/types/lenis.d.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'lenis' {
   export interface LenisOptions {
     duration?: number;
